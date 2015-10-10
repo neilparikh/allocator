4906df66976f168812d13602af90a5e447d245ec
neilparikh <neilparikh@pebble.com>
2015-10-10 12:29:34 -0700

    simplify test.c

--- test.c
+++ test.c
@@ -2,28 +2,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void printIntValue(char arg[], int value);
-
 int main(void)
 {
-	printf("%d\n",sbrk(7));
-	printf("%d\n",sbrk(0));
-	printIntValue()
-	long int read = 0;
-	while (1)
-	{
-		printf("Please enter an integer: ");
-		scanf("%d", &read);
-		char * point = read; 
-		printf("The value of char * at %d is %d\n", read, *point);
-	}
-
-
+	printf("%p\n",sbrk(7));
+	printf("%p\n",sbrk(0));
 }
 
-void printIntValue(char arg[], int value)
-{
-	int length
-	printf()
-
-}
