335071c24d13aa2baad9301826c116efe4112991
Mason Brothers <brothers.mason@gmail.com>
2015-10-10 13:23:13 -0600

    add test file

---
+++ test.c
@@ -0,0 +1,29 @@
+//test.c
+#include <stdio.h>
+#include <unistd.h>
+
+void printIntValue(char arg[], int value);
+
+int main(void)
+{
+	printf("%d\n",sbrk(7));
+	printf("%d\n",sbrk(0));
+	printIntValue()
+	long int read = 0;
+	while (1)
+	{
+		printf("Please enter an integer: ");
+		scanf("%d", &read);
+		char * point = read; 
+		printf("The value of char * at %d is %d\n", read, *point);
+	}
+
+
+}
+
+void printIntValue(char arg[], int value)
+{
+	int length
+	printf()
+
+}
