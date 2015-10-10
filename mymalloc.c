d1dc562f9debb72ee2ee5c76a0610b844f4f11c5
masonbrothers <brothers.mason@gmail.com>
2015-10-10 10:58:21 -0700

    Add file

---
+++ mymalloc.c
@@ -0,0 +1,18 @@
+
+#include <sys/mman.h>
+#include <stdio.h>
+
+void * myMalloc(size_t location);
+
+int main(void) {
+	printf("Works");
+
+
+	return 0;
+}
+
+void * myMalloc(size_t location) {
+	
+
+}
+
